0cff75203ca980602cd6ea0e3c90b75a49e465b2
yosha <derrronny@gmail.com>
2024-09-19 20:35:14 +0300

    First commit

---
+++ utils.js
@@ -0,0 +1,31 @@
+const {Random} = require('random-js');
+const questions = require('./questions.json');
+
+const getRandomQuestion = (topic) => {
+    const random = new Random();
+
+    let questionTopic = topic.toLowerCase();
+
+    if (questionTopic === 'случайный вопрос') {
+        questionTopic = Object.keys(questions)[random.integer(0, Object.keys(questions). length-1)]
+    }
+
+    const randomQuestionIndex = random.integer(0, questions[questionTopic].length-1);
+
+    return {
+        question: questions[questionTopic][randomQuestionIndex],
+        questionTopic,
+    }
+};
+
+const getCorrectAnswer = (topic, id) => {
+    const question = questions[topic].find((question) => question.id === id);
+
+    if (!question.hasOptions) {
+        return question.answer;
+    }
+    
+    return question.options.find((option) => option.isCorrect).text;
+}
+
+module.exports = {getRandomQuestion, getCorrectAnswer};
